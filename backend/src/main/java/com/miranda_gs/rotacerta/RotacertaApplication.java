8847c3f05f033aab1332cb0e6bc4cc897590b7bc
miranda-gs <gabrielmiranda61637@gmail.com>
2026-03-21 21:02:54 -0300

    SpringInitialzr

---
+++ backend/src/main/java/com/miranda_gs/rotacerta/RotacertaApplication.java
@@ -0,0 +1,13 @@
+package com.miranda_gs.rotacerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotacertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotacertaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/miranda_gs/rotacerta/RotacertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miranda_gs.rotacerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotacertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
